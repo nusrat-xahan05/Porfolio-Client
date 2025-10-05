4eeeb6c7a0cdfcf8b944f4bc6cc73fd029b907a3
Nusrat Jahan <nusrat.jahan.az@gmail.com>
2025-10-06 02:06:35 +0600

    Footer Component Added

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,10 +1,17 @@
 
-
 const Footer = () => {
     return (
-        <div className="py-4 bg-amber-300" id="next-section">
-            footer
-        </div>
+        <footer
+            className="py-6 bg-[rgba(7,16,42,0.9)] text-gray-400 text-center text-sm"
+            id="next-section"
+        >
+            <h5 className="font-normal mb-1 text-gray-200">
+                Designed & Developed by <span className="bg-gradient-to-r from-[#FD705C] via-[#FFCFCC] to-[#f3829e] bg-clip-text text-transparent font-semibold">Nusrat Jahan</span>
+            </h5>
+            <p>
+                &copy; {new Date().getFullYear()} All rights reserved.
+            </p>
+        </footer>
     );
 };
 
