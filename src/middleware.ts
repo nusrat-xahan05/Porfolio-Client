48cd29bbb01fb6918ea18644701b840edd28b4b3
Nusrat Jahan <nusrat.jahan.az@gmail.com>
2025-10-09 00:34:10 +0600

    Updated Middleware Function

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,18 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    function middleware(req) {
+        return NextResponse.next();
+    },
+    {
+        pages: {
+            signIn: "/login",
+        },
+    }
+);
 
 export const config = {
-  matcher: ["/dashboard/:path*"],
+    matcher: ["/dashboard/:path*"],
 };
